8dede9996fb19e54de9c3ce6396fc7df108477ee
Imnotcoderdude <topsolo7@naver.com>
2024-05-15 18:32:45 +0900

    수정, 삭제 기능 추가

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -31,6 +31,7 @@ public class MemoController {
 
         return memoresponseDto;
     }
+
     // 조회 하는 API
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
@@ -39,5 +40,30 @@ public class MemoController {
                 .map(MemoResponseDto::new).toList();
 
         return responseList;
+    }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        // 해당 메모가 데이터베이스에 존재하는 지 확인하기
+        if (memoList.containsKey(id)) {
+            Memo memo = memoList.get(id);
+
+            // memo 수정
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        // 해당 메모가 데이터베이스에 존재하는지 확인
+        if (memoList.containsKey(id)) {
+            memoList.remove(id);
+            return id;
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
     }
 }
