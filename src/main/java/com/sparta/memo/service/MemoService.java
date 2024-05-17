769adc326100548c359b5ad6fdbb1541c5e9fc42
Imnotcoderdude <topsolo7@naver.com>
2024-05-17 11:43:06 +0900

    ioc 컨테이너와 bean

--- src/main/java/com/sparta/memo/service/MemoService.java
+++ src/main/java/com/sparta/memo/service/MemoService.java
@@ -4,13 +4,34 @@ import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.entity.Memo;
 import com.sparta.memo.repository.MemoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+// 컴포넌트 에노테이션은 빈을 주입해주기 위해 필요하다.
+@Component
 public class MemoService {
 
+    @Autowired // 생성자가 하나일 경우에는 오토 와이얼드 생략 가능
     private final MemoRepository memoRepository;
+
+    // 수동으로 MemoRepository 에 접근해서 찾아오기
+//    public MemoService(ApplicationContext context) {
+//        // 1. bean 이름으로 가져오기
+////        MemoRepository memoRepository = (MemoRepository) context.getBean("memoRepository");
+
+//        // 2. bean 클래스 형식으로 가져오기
+//        MemoRepository memoRepository = context.getBean(MemoRepository.class);
+//        this.memoRepository = memoRepository;
+//    }
+
+    // 생성자가 아닌 메소드로 초기화를 할 경우에는 final을 사용할 수 없다.
+    // 현업에서도 주로 생성자 주입으로 사용한다.
+//    public void setId(MemoRepository memoRepository) {
+//        this.memoRepository = memoRepository;
+//    }
 
     public MemoService(MemoRepository memoRepository) {
         this.memoRepository = memoRepository;
